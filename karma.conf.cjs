c263636c8afc9744555f833a1a9c081da2e17aa7
Julian Grinblat <julian@dotcore.co.il>
2022-06-03 22:35:46 +0900

    This module is ESM only

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -3,7 +3,6 @@
 // Karma configuration
 // Generated on Wed May 11 2016 23:26:57 GMT+0900 (JST)
 
-process.env.NODE_ENV = 'test';
 if (!process.env.CHROME_BIN) process.env.CHROME_BIN = require('puppeteer').executablePath();
 const IS_REACT_18 = parseInt(require('react').version.split('.')[0], 10) >= 18;
 
@@ -61,9 +60,11 @@ module.exports = (config) => {
     rollupPreprocessor: {
       plugins: [
         require('@rollup/plugin-replace')({
-          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-        }), // this is for react
+          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV), // this is for react
+        }),
         require('@rollup/plugin-babel').default({
+          presets: [['@babel/react', { runtime: 'automatic' }]],
+          plugins: [['istanbul', { include: 'dist/**' }]],
           exclude: 'node_modules/**',
           babelHelpers: 'bundled',
         }),
